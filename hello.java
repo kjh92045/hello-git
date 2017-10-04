82109514a76103e0b09914b4c6967eb56c9c7046
eddie <kjh92045@gmail.com>
2017-10-04 21:48:58 +0900

    Fourth Commit

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class hello{
   public static void main(String[]args){
     System.out.println("Hello.world form GIT");
-	System.out.println("change");
+	   System.out.println("change");
+     System.out.println("Changes after Pushing to remote!")
   }
-
 }
